1fb5fc11689720505215b4aef4050f640e2d0a1b
squirrelzt <squirrelzt@126.com>
2020-01-08 23:19:26 +0800

    react-transition-group动画

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React, { Component, Fragment } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './style.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            list: []
         }
-        this.handleToggle = this.handleToggle.bind(this)
+        this.handleAddItem = this.handleAddItem.bind(this)
     }
-    handleToggle() {
-        this.setState({
-            show: this.state.show ? false : true
-        })
+    handleAddItem() {
+        // this.setState({
+        //     show: this.state.show ? false : true
+        // })
+        this.setState((prevState) => {
+           return {
+               list: [...prevState.list, 'item']
+           }
+        });
     }
     render() {
         return (
             <Fragment>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames='fade'
-                    unmountOnExit
-                    onEntered={(el) => {el.style.color='blue'}}
-                    appear={true}
-                >
-                    <div>hello</div>
-                </CSSTransition>
-                <button onClick={this.handleToggle}>toggle</button>
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <CSSTransition
+                                    in={this.state.show}
+                                    timeout={1000}
+                                    classNames='fade'
+                                    unmountOnExit
+                                    onEntered={(el) => {el.style.color='blue'}}
+                                    appear={true}
+                                    key={index}
+                                >
+                                    <div>{item}</div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
+                <button onClick={this.handleAddItem}>toggle</button>
             </Fragment>
         )
     }
