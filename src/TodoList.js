fb366bee49788b38b70bf79983943c1793b063f7
squirrelzt <squirrelzt@126.com>
2021-02-23 14:04:25 +0800

    redux、store、reducer

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,27 +1,27 @@
 import React, { Component, Fragment } from "react";
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-    'javascript',
-    'java',
-    'golang',
-    'python',
-    'electron',
-];
+import store from "./store";
 
 class TodoList extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+    }
 
     render() {
         return (
             <Fragment>
-                <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}></Input>
-                <Button type='primary'>提交</Button>
+                <Input placeholder='todo info'
+                       style={{width: '300px', marginRight: '10px'}}
+                       value={this.state.inputValue}
+                />
+                <Button type='primary' >提交</Button>
                 <List
                     style={{marginTop: '10px', width: '300px'}}
                     bordered
-                    dataSource={data}
+                    dataSource={this.state.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                 />
             </Fragment>
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+    inputValue: '123',
+    list: [1,2]
+}
+
+export default (state = defaultState, action) => {
+
+    return state;
+}
