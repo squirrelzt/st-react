de4566b67a17461625ccd2a1b1d5718f51146458
squirrelzt <squirrelzt@126.com>
2021-02-24 15:53:25 +0800

    react-redux删除注释

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import 'antd/dist/antd.css';
-import store from "./store";
 import { getInputChangeAction, getAddTodoItemAction, getDeleteTodoItemAction, getInitListAction } from './store/actionCreators';
 import TodoListUI from "./TodoListUI";
 import { connect } from "react-redux";
@@ -9,29 +8,8 @@ class TodoList extends Component {
 
     constructor(props) {
         super(props);
-        // this.state = store.getState();
-        // this.handleInputChange = this.handleInputChange.bind(this);
-        // this.handleStoreChange = this.handleStoreChange.bind(this);
-        // store.subscribe(this.handleStoreChange);
-        // this.handleBtnClick = this.handleBtnClick.bind(this);
-        // this.handleItemDelete = this.handleItemDelete.bind(this);
     }
 
-    // handleStoreChange() {
-    //     this.setState(store.getState())
-    // }
-    // handleInputChange(e) {
-    //     const action = getInputChangeAction(e.target.value);
-    //     store.dispatch(action)
-    // }
-    // handleBtnClick() {
-    //     const action = getAddTodoItemAction();
-    //     store.dispatch(action)
-    // }
-    // handleItemDelete(index) {
-    //     const action = getDeleteTodoItemAction(index);
-    //     store.dispatch(action)
-    // }
     render() {
         const { inputValue, list, handleInputChange, handleBtnClick, handleItemDelete } = this.props;
         return <TodoListUI
@@ -44,9 +22,6 @@ class TodoList extends Component {
     }
 
     componentDidMount() {
-        // const action = getInitListAction();
-        // console.log(action);
-        // store.dispatch(action);
         this.props.initList();
     }
 }
