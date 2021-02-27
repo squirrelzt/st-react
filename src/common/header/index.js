558a771acf4cfc1fd99efd6e501b4dc17627c737
squirrelzt <squirrelzt@126.com>
2021-02-27 21:08:11 +0800

    redux-immutable统一数据格式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -49,7 +49,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.get('focused')
+        // focused: state.get('header').get('focused')
+        focused: state.getIn(['header', 'focused'])
     }
 };
 
