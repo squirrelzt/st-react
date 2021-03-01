fdb982438d9bb5ca8ff3980dca475630e5f76dbe
squirrelzt <squirrelzt@126.com>
2021-03-01 13:44:51 +0800

    react-router-dom使用

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Header from './common/header'
 import store from "./store";
@@ -9,6 +10,10 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <Header/>
+                <BrowserRouter>
+                    <Route path='/' exact render={() => <div>home</div>}></Route>
+                    <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+                </BrowserRouter>
             </Provider>
         )
     }
