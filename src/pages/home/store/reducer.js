003b9b3480c6aed1659c2af10242de3d21d87676
squirrelzt <squirrelzt@126.com>
2021-03-03 23:45:45 +0800

    reducer优化

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -9,20 +9,26 @@ const defaultState = fromJS({
     showScroll: false
 });
 
+const changeHomeData = (state, action) => {
+    return state.merge({
+        topicList: fromJS(action.topicList),
+        articleList: fromJS(action.articleList),
+        recommendList: fromJS(action.recommendList)
+    });
+}
+
+const addArticleList = (state, action) => {
+    return state.merge({
+        articleList: state.get('articleList').concat(action.list),
+        articlePage: action.nextPage
+    })
+}
 export default (state = defaultState, action) => {
     switch (action.type) {
         case actionTypes.CHANGE_HOME_DATA :
-            return state.merge({
-                topicList: fromJS(action.topicList),
-                articleList: fromJS(action.articleList),
-                recommendList: fromJS(action.recommendList)
-            });
+            return changeHomeData(state, action);
         case actionTypes.ADD_ARTICLE_LIST :
-            // return state.set('articleList', state.get('articleList').concat(action.list))
-            return state.merge({
-                articleList: state.get('articleList').concat(action.list),
-                articlePage: action.nextPage
-            })
+            return addArticleList(state, action);
         case actionTypes.TOGGLE_SCROLL_TOP :
             return state.set('showScroll', action.show)
         default :
