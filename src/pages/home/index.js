da3e4b01832c8e1fe28b81f5be7eaa439007cd20
squirrelzt <squirrelzt@126.com>
2021-03-03 22:31:27 +0800

    首页异步获取数据

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import { HomeWrapper, HomeLeft, HomeRight } from './style';
 import Topic from "./components/Topic";
 import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
+import axios from 'axios';
+import * as actionCreators from "../../common/header/store/actionCreators";
 
 class Home extends Component {
+
+    componentDidMount() {
+        axios.get('/api/home.json').then((res)=>{
+            console.log(res.data);
+            const result = res.data.data;
+            console.log(result);
+            const action = {
+                type: 'change_home_data',
+                topicList: result.topicList,
+                articleList: result.articleList,
+                recommendList: result.recommendList
+            };
+            this.props.changeHomeDate(action);
+        })
+    }
 
     render() {
         return (
@@ -27,4 +45,18 @@ class Home extends Component {
     }
 }
 
-export default Home;
+
+const mapStateToProps = (state) => {
+    return {
+        totalPage: state.getIn(['header', 'totalPage']),
+        mouseIn: state.getIn(['header', 'mouseIn'])
+    }
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    changeHomeDate(action) {
+        dispatch(action);
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
