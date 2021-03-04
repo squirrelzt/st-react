89bd2e1a1e10423e29ab44270b96304be11a611f
squirrelzt <squirrelzt@126.com>
2021-03-04 18:32:50 +0800

    redux管理详情页数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import { DetailWrapper, Header, Content } from "./style";
 
 class Detail extends Component {
 
     render() {
+        const { title, content } = this.props;
         return (
             <DetailWrapper>
                 <Header>
-                    张小龙：保持克制做产品
+                    {title}
                 </Header>
-                <Content>
-                    <img src='//upload-images.jianshu.io/upload_images/6313421-c983fdf0a10b892a?imageMogr2/auto-orient/strip|imageView2/2/w/1010/format/webp'/>
-                    <p>做产品经理的同学应该深有体会，任何一个互联网产品都有所谓的场景化设计。</p>
-                    <p>用户在产品中存在不同场景的切换，需要有对应的功能体现。比如在办公场景、回家场景、个人在家场景下，我们家中会在不同场景下提供不同装饰。</p>
-                    <p><b>2.在场景背后，还有一个称呼叫做用户路径。</b></p>
-                    <p>用户路径简单来说可以下面描述：  用户访问产品的起点、过程页面、最终落地页面，这是产品提供的主要功能边界。</p>
-                </Content>
+                <Content dangerouslySetInnerHTML={{__html: content}}></Content>
             </DetailWrapper>
         )
     }
 }
 
-export default Detail;
+
+const mapStateToProps = (state) => ({
+    title: state.getIn(['detail', 'title']),
+    content: state.getIn(['detail', 'content'])
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+        }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
---
+++ src/pages/detail/store/reducer.js
@@ -0,0 +1,19 @@
+import { fromJS } from "immutable";
+import * as actionTypes from './actionTypes';
+
+const defaultState = fromJS({
+    title: '张小龙：保持克制做产品',
+    content: '<img src="//upload-images.jianshu.io/upload_images/6313421-c983fdf0a10b892a?imageMogr2/auto-orient/strip|imageView2/2/w/1010/format/webp"/><p>做产品经理的同学应该深有体会，任何一个互联网产品都有所谓的场景化设计。</p><p>用户在产品中存在不同场景的切换，需要有对应的功能体现。比如在办公场景、回家场景、个人在家场景下，我们家中会在不同场景下提供不同装饰。</p><p><b>2.在场景背后，还有一个称呼叫做用户路径。</b></p><p>用户路径简单来说可以下面描述：  用户访问产品的起点、过程页面、最终落地页面，这是产品提供的主要功能边界。</p>',
+});
+
+
+
+
+export default (state = defaultState, action) => {
+    switch (action.type) {
+
+
+        default :
+            return state;
+    }
+}
